fbdb790f4f657fbac1a69a2f28efea96fb5538d1
Logan Cole <emtlogancole@gmail.com>
2023-02-02 17:56:36 -0600

    create and test node class

---
+++ spec/node_spec.rb
@@ -0,0 +1,28 @@
+require './lib/node'
+
+RSpec.describe Node do
+  it 'exists' do 
+    data = "bloop"
+    node = Node.new(data)
+
+    expect(node).to be_instance_of(Node)
+  end
+end
+
+RSpec.describe Node do
+  it 'has data' do 
+    data = "bloop"
+    node = Node.new(data)
+
+    expect(node.data).to eq("bloop")
+  end
+end
+
+RSpec.describe Node do
+  it 'next node' do 
+    data = "bloop"
+    node = Node.new(data)
+
+    expect(node.next_node).to eq(nil)
+  end
+end
