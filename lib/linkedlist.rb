3a647e4219ce2c3571e60f1ecb9c99f2f8cdc088
Logan Cole <emtlogancole@gmail.com>
2023-02-02 22:02:28 -0600

    change append, count, and linkedlist, still buggy

--- lib/linkedlist.rb
+++ lib/linkedlist.rb
@@ -7,25 +7,26 @@ class LinkedList
   def initialize
     @node = node
     @head = nil
+    @current_node = nil
   end
 
   def append(data)
     if @head
-      @head.next_node = Node.new(data)
+      @current_node.next_node = Node.new(data)
     else
     @head = Node.new(data)
+    @current_node = @head
     end
   end
 
   def count
     counter = 0
-    current_node = @head
     if @head != nil && head.next_node != nil
-      counter += 1
-      counter += 1
-      current_node = head.next_node
+      # require 'pry'; binding.pry
+      counter += 2
+      @current_node = head.next_node
       
-      if current_node.next_node != nil
+      until @current_node.next_node == nil
         counter += 1
       end
       
--- spec/linkedlist_spec.rb
+++ spec/linkedlist_spec.rb
@@ -1,75 +1,77 @@
 require 'rspec'
 require './lib/linkedlist'
 
-RSpec.describe LinkedList do
-  it 'exists' do
+# RSpec.describe LinkedList do
+#   it 'exists' do
 
-    data = "bloop"
-    node = Node.new(data)
-    list = LinkedList.new
+#     data = "bloop"
+#     node = Node.new(data)
+#     list = LinkedList.new
     
-    expect(list).to be_instance_of(LinkedList)
+#     expect(list).to be_instance_of(LinkedList)
 
-  end
-end
+#   end
+# end
+
+# RSpec.describe LinkedList do
+#   it 'has node' do
+#     list = LinkedList.new
+#     list.append("bloop")
+
+#   expect(list.head.data).to eq("bloop")
+#   end
+# end
+
+# RSpec.describe LinkedList do
+#   it 'holds two nodes' do
+
+#     list = LinkedList.new
+#     list.append("bloop")
+#     list.append("flomp")
+
+#   expect(list.head.next_node.data).to eq("flomp")
+#   end
+# end
+
+# RSpec.describe LinkedList do
+#   it 'counts two nodes' do
+
+#     list = LinkedList.new
+#     list.append("bloop")
+#     list.append("flomp")
+
+#   expect(list.count).to eq(2)
+#   end
+# end
+
+# RSpec.describe LinkedList do
+#   it 'counts one node' do
+
+#     list = LinkedList.new
+#     list.append("bloop")
+
+#   expect(list.count).to eq(1)
+#   end
+# end
+
+# RSpec.describe LinkedList do
+#   it 'counts zero nodes' do
+
+#     list = LinkedList.new
+
+#   expect(list.count).to eq(0)
+#   end
+# end
 
 RSpec.describe LinkedList do
-  it 'has node' do
-    list = LinkedList.new
-    list.append("bloop")
-
-  expect(list.head.data).to eq("bloop")
-  end
-end
-
-RSpec.describe LinkedList do
-  it 'holds two nodes' do
-
-    list = LinkedList.new
-    list.append("bloop")
-    list.append("flomp")
-
-  expect(list.head.next_node.data).to eq("flomp")
-  end
-end
-
-RSpec.describe LinkedList do
-  it 'counts two nodes' do
-
-    list = LinkedList.new
-    list.append("bloop")
-    list.append("flomp")
-
-  expect(list.count).to eq(2)
-  end
-end
-
-RSpec.describe LinkedList do
-  it 'counts one node' do
-
-    list = LinkedList.new
-    list.append("bloop")
-
-  expect(list.count).to eq(1)
-  end
-end
-
-RSpec.describe LinkedList do
-  it 'counts zero nodes' do
-
-    list = LinkedList.new
-
-  expect(list.count).to eq(0)
-  end
-end
-
-RSpec.describe LinkedList do
-  it 'counts two nodes' do
+  it 'counts three nodes' do
 
     list = LinkedList.new
     list.append("bloop")
     list.append("flomp")
     list.append("teeth")
+    list.append("awer")
+    require 'pry'; binding.pry
 
   expect(list.count).to eq(3)
   end
