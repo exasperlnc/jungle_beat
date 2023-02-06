7ecbbaeb67c0d99d0b914b8ab08466e7e81b0a69
Logan Cole <emtlogancole@gmail.com>
2023-02-06 12:15:38 -0600

    feat: play

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -4,8 +4,31 @@ class JungleBeat
   attr_accessor :list
 
   def initialize
-  @list = list
+  @list = LinkedList.new
     
   end
+  
+  def append(data)
+    data_array = data.split
+    data_array.map do 
+      list.append(data)
+    end
+
+  end
+
+  def play
+    current_node = @head
+    data_array = []
+    data_array.push current_node
+
+    until current_node.next_node.nil?
+      data_array.push current_node.next_node
+      current_node = current_node.next_node
+    end
+    
+    beats = data_array.join(" ")
+    return `say -r 500 -v Boing #{beats}`
+  end
+
   
 end
